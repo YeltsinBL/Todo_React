33096c1078a4a6b823ec6ffd130deaade1c695ba
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2024-02-28 20:09:58 -0500

    Lógica para verificar si hay ganador y resetear el juego

--- projects/02-tic-tac-toe/README.md
+++ projects/02-tic-tac-toe/README.md
@@ -25,5 +25,18 @@ npm run dev
 - Mostrar el turno dentro de los cuadros de las posiciones.
   - Se crea un nuevo board para actualizar el valor de acuerdo al indice con el turno jugado y se pasa al board inicial para que lo renderice en la vista.
   - Validar que no se sobre escriba en la posición ya jugada en el tablero.
+- Indicar si existe un ganador
+  - Se creó un array con todas las combinaciones ganadoras.
+  - Se creo un estado para el ganador.
+  - Si existe un ganador ya no se podrá seguir jugando.
+  - Se realizó la lógica para verificar si existe un ganador que devuelve el turno actual que ha jugado, de no existir ganador devuelve nulo.
+  - Se muestra un alert si existe el ganador indicando el turno.
+- Resetear juego
+  - Actualizar los estados del board, turn y winner al valor inicial de cada uno.
+  - Se agregó un botón para realizar el reseteo.
 
-> Nota: Los estados se tratan de manera inmutable, se debe de modificar usando el 'set_' pasándole, en este caso, un array nuevo al board principal y este solo se actualizará en donde se ha hecho el clic. Si se actualiza directamente sin utilizar el 'set_' puede que haya error al renderizar.
+> Nota:
+>
+> Los estados se tratan de manera inmutable, se debe de modificar usando el 'set_' pasándole, en este caso, un array nuevo al board principal y este solo se actualizará en donde se ha hecho el clic. Si se actualiza directamente sin utilizar el 'set_' puede que haya error al renderizar.
+>
+> Las actualizaciones de los estados son asíncronos
--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -21,17 +21,31 @@ const Square = ({children, isSelected, updateBoard, index}) => {
   )
 }
 
+// combinaciones ganadoras
+const WINNER_COMBOS = [ 
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
 function App() {
   //tablero con las nueves posiciones vacías usando estado
   const [board, setBoard] = useState(Array(9).fill(null))
 
   // saber el turno siguiente
   const [turn, setTurn] = useState(TURNS.X)
+  // null es que no hay ganador, false es que hay empate
+  const [winner, setWinner] = useState(null)
 
   // cambiar al turno siguiente
   const updateBoard=(index)=> {
     
-    if(board[index]) return // si ya existe un valor, no actualizar
+    if(board[index] || winner) return // si ya existe un valor, no actualizar
 
     const newBoard = [...board]  // Crear un nuevo board
     newBoard[index]=turn// actualizar el valor de la posición del nuevo board
@@ -40,11 +54,37 @@ function App() {
     // cambiar el turno
     const newTurn = turn === TURNS.X? TURNS.O:TURNS.X
     setTurn(newTurn)
+
+    // revisar si hay un ganador
+    const newWinner = checkWinner(newBoard)
+    if(newWinner){
+      setWinner(newWinner)
+    }
   }
 
+  // Lógica para mostrar al ganador
+  const checkWinner = (boardToCheck) =>{
+    for (const combo of WINNER_COMBOS) {
+      const [a,b,c] = combo
+      if(
+        boardToCheck[a] && 
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] == boardToCheck[c]
+      ){
+        return boardToCheck[a]
+      }
+    }
+    return null // si no hay ganador
+  }
+  const resetGame =() =>{
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+  }
   return (
     <main className='board'>
       <h1>Tic Tac Toe</h1>
+      <button onClick={resetGame}>Resetear el juego</button>
       {/* creación del tablero */}
       <section className="game">
         {
@@ -71,6 +111,28 @@ function App() {
           {TURNS.O}
         </Square>
       </section>
+
+         {/* Mostrar al ganador  */}
+        {
+          winner !== null && (
+            <section className="winner">
+              <div className="text">
+              <h2>
+                {
+                  winner === false ? 'Empate' : 'Ganó'
+                }
+              </h2>
+              <header className="win">
+                {winner && <Square>{winner}</Square>}
+              </header>
+              <footer>
+                <button onClick={resetGame}>Empezar de nuevo</button>
+              </footer>
+              </div>
+            </section>
+          )
+        }
+
     </main> 
   )
 }
