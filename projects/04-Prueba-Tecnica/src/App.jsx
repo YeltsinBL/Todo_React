4bc02a4af8b81769e04e6fe1118b214fb0e6c55e
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2024-03-04 01:09:59 -0500

    Separación de la llamada al servicio

--- projects/04-Prueba-Tecnica/src/App.jsx
+++ projects/04-Prueba-Tecnica/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import { getRandomFact } from './services/facts'
 
-const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 // const CAR_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
 const CAT_PREFIX_IMAGE_URL = 'https://cataas.com/cat/'
 
@@ -11,12 +11,7 @@ export function App () {
 
   // para obtener la cita
   useEffect(() => {
-    fetch(CAT_ENDPOINT_RANDOM_FACT)
-      .then(res => res.json()) // cambiar la respuesta a json
-      .then(data => {
-        const { fact } = data
-        setFact(fact)
-      }) // almacenamos en el estado
+    getRandomFact().then(newFact => setFact(newFact))
   }, []) // se ejecuta la primera vez que se renderiza el componente
 
   // recuperar el Id la imagen de la petición anterior
@@ -31,9 +26,15 @@ export function App () {
         setImageId(id)
       })
   }, [fact])
+
+  const handleClick = async () => {
+    const newFact = await getRandomFact()
+    setFact(newFact)
+  }
   return (
     <main>
       <h1>App de gatos</h1>
+      <button onClick={handleClick}>Get new fact</button>
       <section>
         {fact && <p>{fact}</p>}
         {imageId && <img src={`${CAT_PREFIX_IMAGE_URL}${imageId}`} alt={`Imagen extracted using thr first three words for ${fact}`} />}
