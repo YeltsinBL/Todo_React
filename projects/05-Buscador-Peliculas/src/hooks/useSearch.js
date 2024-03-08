641d533c9e33ce83f7ee50ac56436aebe51d8d88
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2024-03-07 19:33:40 -0500

    UseRef para validar el input al entrar por primera vez a la página

--- projects/05-Buscador-Peliculas/src/hooks/useSearch.js
+++ projects/05-Buscador-Peliculas/src/hooks/useSearch.js
@@ -1,10 +1,15 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 
 export function useSearch () {
   const [search, updateSearch] = useState('')
   const [error, setError] = useState(null)
+  const isFirstInput = useRef(true)
 
   useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === ''
+      return
+    }
     if (search === '') {
       setError('No se puede buscar una película vacía')
       return
