65f48b2f0d59568d4ea5f4459674d17cf67d1277
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2024-03-07 19:23:24 -0500

    CustomHooks UseSearch

--- projects/05-Buscador-Peliculas/src/App.jsx
+++ projects/05-Buscador-Peliculas/src/App.jsx
@@ -1,14 +1,12 @@
 import './App.css'
-import { useEffect, useRef, useState } from 'react'
+import { useRef } from 'react'
 import { Movies } from './components/Movies.jsx'
 import { useMovies } from './hooks/useMovies.js'
+import { useSearch } from './hooks/useSearch.js'
 
 function App () {
   const { movies } = useMovies()
-
-  const [query, setQuery] = useState('')
-  const [error, setError] = useState(null)
-
+  const { search, updateSearch, error } = useSearch()
   const inputRef = useRef() // se guardará la referencia del input
 
   const handleSubmit = (event) => {
@@ -16,30 +14,12 @@ function App () {
     // const { query } = Object.fromEntries( // si existiera mas de un input, con esto se agruparían en un json
     //   new window.FormData(event.target) // Recuperar el valor del input
     // )
-    console.log({ query })
+    console.log({ search })
   }
 
   const handleChange = (event) => {
-    const newQuery = event.target.value
-    if (newQuery.startsWith(' ')) return
-    setQuery(event.target.value) // Recuperar el valor del input y actualizamos el estado
+    updateSearch(event.target.value) // Recuperar el valor del input y actualizamos el estado
   }
-
-  useEffect(() => {
-    if (query === '') {
-      setError('No se puede buscar una película vacía')
-      return
-    }
-    if (query.match(/^\d+$/)) {
-      setError('No se puede buscar una película con un numero')
-      return
-    }
-    if (query.length < 3) {
-      setError('La búsqueda debe tener al menos 3 caracteres ')
-      return
-    }
-    setError(null)
-  }, [query])
 
   const handleSubmitWithUseRef = (event) => {
     event.preventDefault() // evitar el comportamiento por defecto
@@ -63,7 +43,7 @@ function App () {
                 border: '1px solid transparent',
                 borderColor: error ? 'red' : 'transparent'
               }}
-              onChange={handleChange} value={query} name='query' type='text' placeholder='Avenger, Star Wars, The Matrix'
+              onChange={handleChange} value={search} name='query' type='text' placeholder='Avenger, Star Wars, The Matrix'
             />
             {/* <input ref={inputRef} type='text' placeholder='Avenger, Star Wars, The Matrix' /> */}
             <button type='submit'>Buscar</button>
