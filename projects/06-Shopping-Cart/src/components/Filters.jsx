c716d072b27bbc3577673d4325d4a5dbe1ce7a69
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2024-03-10 00:30:34 -0500

    Eliminación de la 'fuente de doble verdad': utilizar el valor del context global

--- projects/06-Shopping-Cart/src/components/Filters.jsx
+++ projects/06-Shopping-Cart/src/components/Filters.jsx
@@ -1,17 +1,14 @@
-import { useId, useState } from 'react'
+import { useId } from 'react'
 import './Filters.css'
 import { useFilters } from '../hooks/useFilters'
 
 export function Filters () {
-  const { setFilters } = useFilters()
-
-  const [minPrice, setMinPrice] = useState(0)
+  const { filters, setFilters } = useFilters()
 
   const minPriceFilterId = useId()
   const categoryFilterId = useId()
 
   const handleChangeMinPrice = (event) => {
-    setMinPrice(event.target.value)
     setFilters(prevState => ({
       ...prevState,
       minPrice: event.target.value
@@ -29,8 +26,12 @@ export function Filters () {
     <section className='filters'>
       <div>
         <label htmlFor={minPriceFilterId}>Precio a partir de </label>
-        <input type='range' id={minPriceFilterId} min='0' max='1000' onChange={handleChangeMinPrice} />
-        <span>{minPrice}</span>
+        <input
+          type='range' id={minPriceFilterId}
+          min='0' max='1000' onChange={handleChangeMinPrice}
+          value={filters.minPrice}
+        />
+        <span>{filters.minPrice}</span>
       </div>
       <div>
         <label htmlFor={categoryFilterId}>Categoría</label>
