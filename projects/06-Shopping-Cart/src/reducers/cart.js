4f3b10f6068440d82709b550c818de25d2db91c8
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2024-03-12 03:34:20 -0500

    Cambiar el Switch por objetos para actualizar el estado de los productos del carrito

--- projects/06-Shopping-Cart/src/reducers/cart.js
+++ projects/06-Shopping-Cart/src/reducers/cart.js
@@ -11,46 +11,48 @@ const CART_ACTION_TYPES = {
 
 }
 
+const UPDATE_STATE_BY_ACTION = {
+  [CART_ACTION_TYPES.ADD_TO_CART]: (state, action) => {
+    const { id } = action.payload
+    const productInCartIndex = state.findIndex(
+      item => item.id === id
+    )
+    // si el producto se encuentra agregado, aumentar solo la cantidad
+    if (productInCartIndex >= 0) {
+      // utilizando structureClone:hace una copia profunda del array
+      const newState = structuredClone(state)
+      newState[productInCartIndex].quantity += 1
+      updateLocalStorage(newState)
+      return newState
+    }
+    const newState = [
+      ...state,
+      {
+        ...action.payload, // productos
+        quantity: 1
+      }
+    ]
+
+    updateLocalStorage(newState)
+    return newState
+  },
+  [CART_ACTION_TYPES.REMOVE_FROM_CART]: (state, action) => {
+    const { id } = action.payload
+    const newState = state.filter(item => item.id !== id)
+
+    updateLocalStorage(newState)
+    return newState
+  },
+  [CART_ACTION_TYPES.CLEAR_CART]: () => {
+    updateLocalStorage([])
+    return []
+  }
+}
+
 // UseReducer
 export const cartReducer = (state, action) => {
   // Pasamos la acción que va hacer y todo el objeto para actualizar el estado (producto)
-  const { type: actionType, payload: actionPayload } = action
-  switch (actionType) {
-    case CART_ACTION_TYPES.ADD_TO_CART: {
-      const { id } = actionPayload
-      const productInCartIndex = state.findIndex(
-        item => item.id === id
-      )
-      // si el producto se encuentra agregado, aumentar solo la cantidad
-      if (productInCartIndex >= 0) {
-      // utilizando structureClone:hace una copia profunda del array
-        const newState = structuredClone(state)
-        newState[productInCartIndex].quantity += 1
-        updateLocalStorage(newState)
-        return newState
-      }
-      const newState = [
-        ...state,
-        {
-          ...actionPayload,
-          quantity: 1
-        }
-      ]
-
-      updateLocalStorage(newState)
-      return newState
-    }
-    case CART_ACTION_TYPES.REMOVE_FROM_CART: {
-      const { id } = actionPayload
-      const newState = state.filter(item => item.id !== id)
-
-      updateLocalStorage(newState)
-      return newState
-    }
-    case CART_ACTION_TYPES.CLEAR_CART: {
-      updateLocalStorage([])
-      return []
-    }
-  }
-  return state
+  const { type: actionType } = action
+  const updateState = UPDATE_STATE_BY_ACTION[actionType]
+  return updateState ? updateState(state, action) : state
 }
