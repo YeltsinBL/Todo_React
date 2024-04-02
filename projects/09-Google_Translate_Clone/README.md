040b8e7959acf3552694f6d0b8c1639c974f0320
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2024-04-01 23:42:48 -0500

    Agregar Bootstrap y visualizar datos iniciales usando el CustomHook del Reducer

--- projects/09-Google_Translate_Clone/README.md
+++ projects/09-Google_Translate_Clone/README.md
@@ -8,3 +8,5 @@ Utilización React + TypeScript + Vite
 - Agregar las constantes de los distintos idiomas y el por defecto.
 - Tipar los idiomas, el estado del de la traducción y las acciones.
 - Crear un UseReducer y CustomHook.
+- Agregar Bootstrap y visualizar datos iniciales usando el CustomHook del Reducer
+  - Utilice el siguiente comando: `npm i react-bootstrap -E --legacy-peer-deps`
--- projects/09-Google_Translate_Clone/src/App.tsx
+++ projects/09-Google_Translate_Clone/src/App.tsx
@@ -1,11 +1,36 @@
+// import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container, Row, Col, Button } from 'react-bootstrap'
+
 import './App.css'
+import { useStore } from './hooks/useStore'
+import { AUTO_LANGUAGE } from './consts.ts'
+import { ArrowIcon } from './components/Icons.tsx'
+
 
 function App() {
 
+  const { fromLanguage, toLanguage, interchangeLanguages } = useStore()
+
   return (
-    <>
+    <Container fluid>
       <h1>Google Translate</h1>
-    </>
+      <Row>
+        <Col>
+          <h2>From</h2>
+          {fromLanguage}
+        </Col>
+        <Col>
+          <Button variant='link' disabled={fromLanguage === AUTO_LANGUAGE} onClick={interchangeLanguages}>
+            <ArrowIcon />
+          </Button>
+        </Col>
+        <Col>
+          <h2>To</h2>
+          {toLanguage}
+        </Col>
+      </Row>
+
+    </Container>
   )
 }
 
